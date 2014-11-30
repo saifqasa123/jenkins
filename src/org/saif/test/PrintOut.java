2122564c3ee3b7b68337502963c02033dd812340
saif.qasaimeh <saif_qasa123@yahoo.com>
2014-11-29 20:47:30 -0600

    Jenkins project

---
+++ src/org/saif/test/PrintOut.java
@@ -0,0 +1,12 @@
+package org.saif.test;
+
+/**
+ * @author saif
+ *
+ */
+public class PrintOut {
+	
+	public void getName(){
+		System.out.println("my namd is shitt");
+	}
+}
---
+++ test/org/saif/test/PrintOutTest.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package org.saif.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author saif
+ *
+ */
+public class PrintOutTest {
+
+	PrintOut printOut = new PrintOut();
+	/**
+	 * Test method for {@link org.saif.test.PrintOut#getName()}.
+	 */
+	@Test
+	public void testGetName() {
+		printOut.getName();
+	}
+
+}
